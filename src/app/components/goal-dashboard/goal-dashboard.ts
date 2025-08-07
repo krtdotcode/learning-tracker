1a9f063370e66ced1fb217adbaa6ef3b58f5b9bd
Kurt Joshua Cayaga <kurtjoshuacayaga@gmail.com>
2025-08-07 20:28:53 +0800

    feat: add learning goal input form and list

--- src/app/components/goal-dashboard/goal-dashboard.ts
+++ src/app/components/goal-dashboard/goal-dashboard.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-goal-dashboard',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './goal-dashboard.html',
   styleUrl: './goal-dashboard.css'
 })
 export class GoalDashboard {
+  goals: string[] = [];
+  newGoalTitle: string = '';
+  showForm: boolean = false;
 
+  toggleForm() {
+    this.showForm = !this.showForm;
+  }
+
+  addGoal() {
+    if (this.newGoalTitle.trim()) {
+      this.goals.push(this.newGoalTitle.trim());
+      this.newGoalTitle = '';
+      this.showForm = false;
+    }
+  }
 }
